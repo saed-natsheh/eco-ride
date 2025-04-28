2acd1c369db018a04b67cd6b592b3ac914406111
saed-natsheh <sdalnatsheh@gmail.com>
2025-04-29 00:07:46 +0200

    start layout

---
+++ eco-ride/resources/views/layout/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Eco Rides</title>
+    @yield('css')
+</head>
+
+<body>
+    @yield('content')
+
+    @yield('script')
+</body>
+
+</html>
