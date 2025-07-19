13ed9bcfac26d8545b2af8db82e0b2e96bdf6e8c
saed-natsheh <sdalnatsheh@gmail.com>
2025-07-19 21:35:21 +0200

    mettre à jour

--- eco-ride/database/migrations/0001_01_01_000000_create_users_table.php
+++ eco-ride/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('role')->default('user');
-            $table->boolean('suspended')->default(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
